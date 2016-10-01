40edea12a64768504d9dd782452db40a1b8ff631
Jelldrm <Jelle2510@gmail.com>
2016-10-01 21:24:12 +0200

    Added health checker + integrationtest

---
+++ src/main/java/com/weblogicmonitoring/health/ApplicationHealthChecker.java
@@ -0,0 +1,14 @@
+package com.weblogicmonitoring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationHealthChecker {
+
+    @RequestMapping("/webmon")
+    public String isApplicationRunning(){
+        return "Weblogic Monitoring application is running!";
+    }
+
+}

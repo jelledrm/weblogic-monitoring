0325d0b472983b092e11e90274bee276bbc5579a
Jelldrm <Jelle2510@gmail.com>
2016-10-01 17:34:07 +0200

    Basic spring boot project with Apache Camel

---
+++ src/main/java/com/weblogicmonitoring/WeblogicMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.weblogicmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogicMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblogicMonitoringApplication.class, args);
+	}
+}
